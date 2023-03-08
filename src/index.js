d1beff58a7170af26bef5ad3c7558a901b71ad25
Jordan <camiloarmijosantifonamusic@gmail.com>
2023-03-08 17:51:05 -0500

    Implement "Submit" button functionality to save a score in the API

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import './style.css';
 
 const refreshButton = document.getElementById('refresh-btn');
 const scoresListEl = document.getElementById('score-list-container');
+const nameInput = document.getElementById('name-input');
+const scoreInput = document.getElementById('score-input');
+const formEl = document.getElementById('add-form');
 
 const gameId = '0w1DIhTP3YxPLlnbxiIx'
 const URL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`;
@@ -35,4 +38,24 @@ const renderScores = () => {
     });
 }
 
+const saveScore = (url = URL) => {
+    const nameValue = nameInput.value
+    const scoreValue = nameInput.value
+
+    fetch(URL, {
+        method: 'POST',
+        body: JSON.stringify(
+            {
+                "user": nameValue,
+                "score": scoreValue
+            }
+        ),
+        headers: { 'Content-type': 'application/json; charset=UTF-8', },
+    })
+}
+
 refreshButton.addEventListener('click', renderScores)
+formEl.addEventListener('submit', (e) => {
+    e.preventDefault()
+    saveScore()
+})
