433efdf61b6a5eac15eedb5974d0a46254099b90
Jordan <camiloarmijosantifonamusic@gmail.com>
2023-03-08 17:24:41 -0500

    Add functionality to render all scores when pressing "Refresh" button

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './index.html';
 import './style.css';
 
 const refreshButton = document.getElementById('refresh-btn');
+const scoresListEl = document.getElementById('score-list-container');
 
 const gameId = '0w1DIhTP3YxPLlnbxiIx'
 const URL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`;
@@ -11,6 +12,27 @@ const getScores = async (url = URL) => {
     const scoresJson = await scores.json()
     
     console.log(json)
+    return scoresJson
 }
 
-refreshButton.addEventListener('click', getScores)
+const renderScores = () => {
+    const scoresObj = getScores()
+    const scoresArray = scoresObj.result
+
+    scoresArray.forEach(score => {
+        let scoreName = score.user
+        let scoreNum = score.score
+        let scoreHTML = `
+            <div class="user-info">
+                <div class="name-wrapper">                            
+                    <p class="user-name">${scoreName}</p>
+                    <span class="divider-colon">:&nbsp;</span>
+                </div>
+                <p class="user-score">${scoreNum}</p>
+            </div>`
+
+        scoresListEl.insertAdjacentHTML('beforeend', scoreHTML)
+    });
+}
+
+refreshButton.addEventListener('click', renderScores)
