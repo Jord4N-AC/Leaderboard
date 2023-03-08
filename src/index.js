f672ef5702c565d23825412d1a8e22bdba04b40f
Jordan <camiloarmijosantifonamusic@gmail.com>
2023-03-08 16:18:21 -0500

    Implement "Refresh" button functionality to get all scores from API and parse the JSON

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import './index.html';
 import './style.css';
 
+const refreshButton = document.getElementById('refresh-btn');
+
 const gameId = '0w1DIhTP3YxPLlnbxiIx'
+const URL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores/`;
+
+const getScores = async (url = URL) => {
+    const scores = await fetch(url)
+    const scoresJson = await scores.json()
+    
+    console.log(json)
+}
+
+refreshButton.addEventListener('click', getScores)
