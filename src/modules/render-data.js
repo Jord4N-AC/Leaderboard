6f7638ee452c6d3c8220e568a5c2413b7b7ad112
Jordan <camiloarmijosantifonamusic@gmail.com>
2023-03-10 11:00:36 -0500

    Split function to render scores into more functions to make code more functional, export

--- src/modules/render-data.js
+++ src/modules/render-data.js
@@ -1,25 +1,29 @@
 import getScores from './get-data.js';
 import { scoresListEl, URL } from './variables.js';
 
+const renderScore = ({ user, score }) => {
+    const scoreHTML = `
+        <div class="user-info">
+            <div class="name-wrapper">
+                <p class="user-name">${user}</p>
+                <span class="divider-colon">:&nbsp;</span>
+            </div>
+            <p class="user-score">${score}</p>
+        </div>`;
 
-const renderScores = async () => {
-    const scoresObj = await getScores(URL);
-    const scoresArray = scoresObj.result;
+    scoresListEl.insertAdjacentHTML('beforeend', scoreHTML);
+}
 
-    scoresArray.forEach((score) => {
-        const scoreName = score.user;
-        const scoreNum = score.score;
-        const scoreHTML = `
-            <div class="user-info">
-                <div class="name-wrapper">
-                    <p class="user-name">${scoreName}</p>
-                    <span class="divider-colon">:&nbsp;</span>
-                </div>
-                <p class="user-score">${scoreNum}</p>
-            </div>`;
-
-        scoresListEl.insertAdjacentHTML('beforeend', scoreHTML);
-    });
+const renderAllScores = (arrayData) => {
+    arrayData.forEach((score) => renderScore(score));
 };
 
-export default renderScores;
+const getAndRenderAllScores = async () => {
+    const scoresObj = await getScores(URL);
+    const scoresArray = scoresObj.result;
+    
+    scoresListEl.innerHTML = '';
+    renderAllScores(scoresArray);
+};
+
+export default getAndRenderAllScores;
